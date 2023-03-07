cc4e89736e1b25d17fa52a88133408c86333d65a
akifisitan <akifisitan@hotmail.com>
2023-03-08 00:55:13 +0300

    Modularize GMPBot
    - Load extensions from the extensions folder

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import nextcord.ext
 from nextcord.ext.commands import Bot
 from config import BOT_TOKEN, CHECK
@@ -8,7 +9,8 @@ intents.message_content = True
 intents.members = True
 intents.dm_messages = False  # Stop listening to DMs
 activity = nextcord.Activity(name="TV", type=nextcord.ActivityType.watching)
-bot = Bot(command_prefix="!gmp", intents=intents, activity=activity)
+bot = Bot(command_prefix="?", intents=intents, activity=activity)
+bot.remove_command("help")
 
 
 @bot.event
@@ -16,10 +18,12 @@ async def on_ready():
     print(f"Connected to bot: {bot.user.name}")
 
 
-# Run the bot if everything is set up correctly
 def main():
-    if CHECK:
-        bot.run(BOT_TOKEN)
+    if not CHECK:
+        return
+    for extension in filter(lambda x: x.endswith(".py"), os.listdir("extensions")):
+        bot.load_extension(f"extensions.{extension[:-3]}")
+    bot.run(BOT_TOKEN)
 
 
 if __name__ == '__main__':
